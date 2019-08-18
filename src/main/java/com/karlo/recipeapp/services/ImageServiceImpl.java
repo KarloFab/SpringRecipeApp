e0791e686dd7eae95c0f408fc575608a3c1498d4
KarloFab <fabijanickarlo1@gmail.com>
2019-08-18 16:40:46 +0200

    Added ImageServiceImplTest. Added method for saving images into DB.

--- src/main/java/com/karlo/recipeapp/services/ImageServiceImpl.java
+++ src/main/java/com/karlo/recipeapp/services/ImageServiceImpl.java
@@ -1,12 +1,41 @@
 package com.karlo.recipeapp.services;
 
+import com.karlo.recipeapp.domain.Recipe;
+import com.karlo.recipeapp.repositories.RecipeRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.Optional;
+
 @Service
 public class ImageServiceImpl implements ImageService {
-    @Override
-    public void saveImageFile(Long valueOf, MultipartFile image) {
 
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    public void saveImageFile(Long recipeId, MultipartFile file) {
+
+        try{
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] fileBytes = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for(byte b: file.getBytes()){
+                fileBytes[i++] = b;
+            }
+
+            recipe.setImage(fileBytes);
+
+            recipeRepository.save(recipe);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
