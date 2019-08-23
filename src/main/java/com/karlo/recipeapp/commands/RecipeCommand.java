f67c786db99242537c79bf1054b0639a285152c0
KarloFab <fabijanickarlo1@gmail.com>
2019-08-23 22:59:49 +0200

    Added bean validations

--- src/main/java/com/karlo/recipeapp/commands/RecipeCommand.java
+++ src/main/java/com/karlo/recipeapp/commands/RecipeCommand.java
@@ -2,6 +2,12 @@ package com.karlo.recipeapp.commands;
 
 import com.karlo.recipeapp.domain.enums.Difficulty;
 import lombok.*;
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -10,12 +16,28 @@ import java.util.Set;
 @NoArgsConstructor
 public class RecipeCommand {
     private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
     private String description;
+
+    @Min(1)
+    @Max(999)
     private Integer prepTime;
+
+    @Min(1)
+    @Max(999)
     private Integer cookTime;
+
+    @Min(1)
+    @Max(100)
     private Integer servings;
     private String source;
+
+    @URL
     private String url;
+
+    @NotBlank
     private String directions;
     private Set<IngredientCommand> ingredients = new HashSet<>();
     private Difficulty difficulty;
